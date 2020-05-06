8d28c9cca63a1ddebeab52d0f5044226d7057a29
Oleksandr Chura <oleksandr.chura1@ibm.com>
2020-05-06 14:19:56 +0200

    final fix

--- transfer-repo.sh
+++ transfer-repo.sh
@@ -1,61 +1,20 @@
 #!/bin/bash
-# Sometimes you need to move your existing git repository
-# to a new remote repository (/new remote origin).
-# Here are a simple and quick steps that does exactly this.
-#
-# Let's assume we call "old repo" the repository you wish
-# to move, and "new repo" the one you wish to move to.
-#
-### Step 1. Make sure you have a local copy of all "old repo"
-### branches and tags.
-# Fetch all of the remote branches and tags:
-# git fetch origin
 
-# View all "old repo" local and remote branches:
-# git branch -a
-
-# If some of the remotes/ branches doesn't have a local copy,
-# checkout to create a local copy of the missing ones:
-# git checkout -b <branch> origin/<branch>
-
-# Now we have to have all remote branches locally.
 branch_name_ibm=$1
 branch_name_target=$2
-
-git checkout $branch_name_ibm
-git pull
-
-### Step 2. Add a "new repo" as a new remote origin:
+# add new-origin pekao repo
 git remote add new-origin https://github.com/olesj98/new-test-transfer.git
-
-# git push -u new-origin BRANCHNAME -f
-git checkout -b $branch_name_target
-git pull --rebase origin $branch_name_ibm
-git push -u new-origin $branch_name_target -f
+# checkout to target ibm branch
 git checkout $branch_name_ibm
+# pull from target ibm branch
+git pull
+# create and checkout to new temporary local branch with pekao target name
+git checkout -b $branch_name_target
+# pull from ibm target branch
+git pull --rebase origin $branch_name_ibm
+# push to upstream new branch in pekao repo ??upstream??
+git push -u new-origin $branch_name_target -f
+# checkout to ibm target branch
+git checkout $branch_name_ibm
+# delete temporary pekao target branch
 git branch -d $branch_name_target
-### Step 3. Push all local branches and tags to a "new repo".
-# Push all local branches (note we're pushing to new-origin):
-# ====
-# git branch -D master
-
-# git push -f new-origin master
-# ====
-
-# Push all tags:
-# git push --tags new-origin
-
-
-### Step 4. Remove "old repo" origin and its dependencies.
-# View existing remotes (you'll see 2 remotes for both fetch and push)
-# git remote -v
-
-# Remove "old repo" remote:
-# git remote rm origin
-
-# Rename "new repo" remote into just 'origin':
-# git remote rename new-origin origin
-
-
-### Done! Now your local git repo is connected to "new repo" remote
-### which has all the branches, tags and commits history.
